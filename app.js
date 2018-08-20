952573401e24b3303d0c4d45da68116b1b91c27d
Bradley Zerafa <bradleyzerafa1@hotmail.com>
2018-08-20 16:24:02 +1000

    added functionality to dice roll (not complete)

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ GAME RULES:
 
 */
 
+var currentNumb = 0;
+var currentPlayer = 1;
 
 
 var newGameBtn = document.getElementById("newgame");
@@ -17,15 +19,73 @@ var player2Score = document.getElementById("score-1");
 var player1Current = document.getElementById("current-0");
 var player2Current = document.getElementById("current-1");
 var diceImg = document.getElementById("dice");
+var rollDiceBtn = document.getElementById("rolldice");
+var currentScore1 = document.getElementById("current-0");
+var currentScore2 = document.getElementById("current-1");
+var active0 = document.getElementById("active-0");
+var active1 = document.getElementById("active-1");
 
 
 newGameBtn.addEventListener("click", gameReset);
+rollDiceBtn.addEventListener("click", diceRoll);
 
+
+
+
+function holdPoints(){
+
+}
 
 
 function diceRoll(){
-  
+  var diceNumb = Math.floor(Math.random() * 6) + 1;
+
+if (diceNumb >= 2){
+  if (diceNumb == 2){
+    diceImg.src = "dice-2.png";
+  } else if (diceNumb == 3){
+    diceImg.src = "dice-3.png";
+  } else if (diceNumb == 4){
+    diceImg.src = "dice-4.png";
+  } else if (diceNumb == 5){
+    diceImg.src = "dice-5.png";
+  } else {
+    diceImg.src = "dice-6.png";
+  }
+  diceImg.style.visibility = "";
+  currentNumb = currentNumb + diceNumb;
+
+  if (currentPlayer == 1){
+    currentScore1.innerHTML = currentNumb;
+  } else {
+    currentScore2.innerHTML = currentNumb;
+  }
+
+} else {
+    diceImg.style.visibility = "hidden";
+    currentNumb = 0;
+
+    if (currentPlayer == 1){
+      currentScore1.innerHTML = currentNumb;
+      currentPlayer = 2;
+      active0.classList.remove = "active player-name::after";
+      active1.className = " active";
+
+
+    } else {
+      currentScore2.innerHTML = currentNumb;
+      currentPlayer = 1;
+
+    }
+  }
+
+
+  console.log(diceNumb);
+  console.log(currentNumb);
+
 }
+
+
 
 
 function gameReset(){
@@ -34,4 +94,5 @@ function gameReset(){
   player1Current.innerHTML = 0;
   player2Current.innerHTML = 0;
   diceImg.style.visibility = "hidden";
+  currentNumb = 0;
 };
